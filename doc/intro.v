cc33b7027ad4d6c7c06f7cf8a1810b7fc4dc9085
Matthieu Sozeau <mattam@mattam.org>
2009-11-05 17:56:53 -0500

    Introduction continued.

--- doc/intro.v
+++ doc/intro.v
@@ -1,6 +1,16 @@
+(** * A gentle introduction to [Equations] 
+
+   [Equations] is a plugin for Coq%\footnote{Currently available for the trunk version only}
+   \cite{Coq}% that comes with a few support modules defining classes and tactics for 
+   running it. We will introduce its main features through a handful of examples, requiring 
+   no prior knowledge of the tool. We start our Coq primer session by importing the [Equations]
+   module.
+   *)
+
 Require Import Equations.
-Require Import List.
+
 (* begin hide *)
+Check @eq.
 Ltac funind c Hcall ::= 
   match c with
     appcontext C [ ?f ] => 
@@ -26,16 +36,17 @@ Ltac funelim c :=
         (let prf := eval simpl in x in
           dependent_pattern c ; apply prf)
   end.
+Require Import Bvector.
 
-Derive DependentElimination for nat bool option sum prod list.
+Derive DependentElimination for nat bool option sum prod list vector.
 (* end hide *)
+
 (** ** Inductive types
 
-   In its simplest form, Equations permits to define functions on inductive datatypes.
+   In its simplest form, [Equations] allows to define functions on inductive datatypes.
    Take for example the booleans defined as an inductive type with two constructors [true] and [false]:
-   [[
-   Inductive bool : Set := true : bool | false : bool
-   ]] 
+
+   [Inductive bool : Set := true : bool | false : bool]
    
    We can define the boolean negation as follows: *)
 
@@ -50,17 +61,14 @@ Check neg_ind_equation_2.
 
 Lemma neg_inv : forall b, neg (neg b) = b.
 Proof. intros b. funelim (neg b); simp neg. Defined.
-
 (* end hide *)
-
 (** [Equations] declarations are formed by a signature definition and a set of _clauses_ 
    that must form a _covering_ of this signature. The compiler is then expected to
    automatically find a corresponding case-splitting tree that implements the function.
    In simple cases like this, it simply needs to split on the single variable [b] to
    produce two new _programming problems_ [neg true] and [neg false] that are directly 
    handled by the user clauses. We will see in more complex examples that this search
-   for a splitting tree may be non-trivial.
- *)
+   for a splitting tree may be non-trivial. *)
 
 (** ** Reasoning principles
 
@@ -78,25 +86,19 @@ Proof. intros b. funelim (neg b); simp neg. Defined.
    inductive graph of any [Equations] definition, giving the strongest induction 
    principle on the function. 
 
-   I.e., for [neg] the inductive graph is defined as:
-   [[
+   I.e., for [neg] the inductive graph is defined as: [[
+
    Inductive neg_ind : forall b : bool, neg_comp b -> Prop :=
    | neg_ind_equation_1 : neg_ind true false
-   | neg_ind_equation_2 : neg_ind false true
-   ]]
-
-   Along with a proof that [Π b, neg_ind b (neg b)], we can eliminate any call
+   | neg_ind_equation_2 : neg_ind false true ]]
+   Along with a proof of [Π b, neg_ind b (neg b)], we can eliminate any call
    to [neg] specializing its argument and result in a single command. 
    Suppose we want to show that [neg] is involutive for example, our goal will 
-   look like:
-   
-   [[
+   look like: [[
+
   b : bool
   ============================
-   neg (neg b) = b
-   ]]
-   
-
+   neg (neg b) = b ]]
    An application of the [funind] tactic will produce two goals corresponding to 
    the splitting done in [neg]: [neg false = true] and [neg true = false].
    These correspond exactly to the rewriting lemmas generated for [neg].
@@ -141,16 +143,13 @@ app A (cons a l) l' := cons a (app l l').
 (** Recursive definitions like [app] can be unfolded easily so proving the 
    equations as rewrite rules is direct. The induction principle associated 
    to this definition is more interesting however. We can derive from it the 
-   following _elimination_ principle for calls to [app]:
-   [[
-app_elim :
-forall P : forall (A : Type) (l l' : list A), app_comp l l' -> Prop,
-(forall (A : Type) (l' : list A), P A nil l' l') ->
-(forall (A : Type) (a : A) (l l' : list A),
- P A l l' (app l l') -> P A (a :: l) l' (a :: app l l')) ->
-forall (A : Type) (l l' : list A), P A l l' (app l l')
-]]
-
+   following _elimination_ principle for calls to [app]: [[
+   app_elim :
+   forall P : forall (A : Type) (l l' : list A), app_comp l l' -> Prop,
+   (forall (A : Type) (l' : list A), P A nil l' l') ->
+   (forall (A : Type) (a : A) (l l' : list A),
+   P A l l' (app l l') -> P A (a :: l) l' (a :: app l l')) ->
+   forall (A : Type) (l l' : list A), P A l l' (app l l') ]]
   Using this eliminator, we can write proofs exactly following the 
   structure of the function definition, instead of redoing the splitting 
   by hand. This idea is already present in the [Function] package 
@@ -167,7 +166,7 @@ Check app_ind. Check @app_comp. Check @app_ind_equation_1. Check @app_ind_equati
 
    The structure of real programs is richer than a simple case tree on the 
    original arguments in general. In the course of a computation, we might 
-   want to scrutinize intermediate results (e.g. comming from function calls)
+   want to scrutinize intermediate results (e.g. coming from function calls)
    to produce an answer. This literally means adding a new pattern to the left of
    our equations made available for further refinement. This concept is know as with
    clauses in the Agda %\cite{norell:thesis}% community and was first presented 
@@ -212,43 +211,147 @@ unzip A B (cons p l) <= unzip l => {
 Equations equal (n m : nat) : { n = m } + { n <> m } :=
 equal O O := left eq_refl ;
 equal (S n) (S m) <= equal n m => {
-  equal (S n) (S ?(n)) (left eq_refl) := left eq_refl ;
+  equal (S n) (S n) (left eq_refl) := left eq_refl ;
   equal (S n) (S m) (right p) := right _ } ;
 equal x y := right _.
 
 (** Of particular interest here is the inner program refining the recursive result.
    As [equal n m] is of type [{ n = m } + { n <> m }] we have two cases to consider:
-
-   - Either we are in the [left p] case, and we know that [p] is a proof that [n = m],
+   
+   - Either we are in the [left p] case, and we know that [p] is a proof of [n = m],
      in which case we can do a nested match on [p]. The result of matching this equality
      proof is to unify [n] and [m], hence the left hand side patterns become [S n] and
      [S ?(n)] and the return type of this branch is refined to [{ n = n } + { n <> n }].
      We can easily provide a proof for the left case. 
      
    - In the right case, we mark the proof unfilled with an underscore. This will
-     generate an obligation for this hole, that can be filled automatically by a 
+     generate an obligation for the hole, that can be filled automatically by a 
      predefined tactic or interactively by the user in proof mode (this uses the
      same obligation mechanism as the Program extension
-     %\cite{sozeau.Coq/FingerTrees/article}%).
-*)
+     %\cite{sozeau.Coq/FingerTrees/article}%). In this case the automatic tactic 
+     is able to derive by itself that [n <> m -> S n <> S m].
 
-(** The [tail] function is made total by returning the dummy element [nil] in the 
-   first equation, but we might like to express that such partial functions are
-   in fact total, given a precondition on their input. *)
-(* Equations head A (l : list A | l <> nil) : A := *)
-(* head A (exist nil pf) :=! pf; *)
-(* head A (exist (cons a v) _) := a. *)
+   Dependent types are also useful to turn partial functions into total functions by
+   restricting their domain. Typically, we can force the list passed to [head] 
+   to be non-empty using the specification:
+*)
 
 Equations head A (l : list A) (pf : l <> nil) : A :=
 head A nil pf :=! pf;
 head A (cons a v) _ := a.
 
-Next Obligation. depelim l. impossible_call head. constructor. Defined.
+(** We decompose the list and are faced with two cases:
 
-(** ** Inductive families *)
+   - In the first case, the list is empty, hence the proof [pf] of type 
+     [nil <> nil] allows us to derive a contradiction. We make use of
+     another category of right-hand sides, which we call _empty_ nodes
+     to inform the compiler that a contradiction is derivable in this case.
+     In general we cannot expect the compiler to find by himself that 
+     the context contains a contradiction, as it is undecidable 
+     %\cite{DBLP:conf/plpv/Oury07,DBLP:conf/birthday/GoguenMM06}%.
+   - In the second case, we simply return the head of the list, disregarding
+     the proof.
+ *)
 
-Equations K {A} (x : A) (P : x = x -> Type) (p : P eq_refl) (H : x = x) : P H :=
-K A x P p eq_refl := p.
+(** *** Inductive families
+
+   The next step is to make constraints such as non-emptiness part of the 
+   datatype itself. This capability is provided through inductive families in
+   Coq %\cite{paulin93tlca}%, which are a similar concept to the recent generalization 
+   of algebraic datatypes to GADTs in functional languages like Haskell 
+   %\cite{ghani-popl07}%. Families provide a way to associate to each constructor 
+   a different type, making it possible to give specific information about a value 
+   in its type. 
+
+   **** Equality 
+   The alma mater of inductive families is the propositional equality 
+   [eq] defined as: [[
+   Inductive eq (A : Type) (x : A) : A -> Prop := 
+     eq_refl : eq A x x. ]]
+   
+   It is a central tool in the compilation process so we present it in detail here.
+   It is also a contentious subject in the type theory community, we'll discuss 
+   it in section %\ref{sec:equality}%.
+
+   Equality is a polymorphic relation on [A]. (The [Prop] sort (or kind) categorizes
+   propositions, while the [Set] sort, equivalent to [*] in Haskell categorizes 
+   computational objects.) Equality is _parameterized_ by a value [x] of type [A] and 
+   _indexed_ by another value of type [A]. Its single constructor states that 
+   equality is reflexive, so the only way to build an object of [eq x y] is if 
+   [x ~= y], that is if [x] is definitionaly equal to [y]. 
+   
+   Now what is the elimination principle associated to this inductive family?
+   It is the good old Leibniz substitution principle:
+
+   [forall (A : Type) (x : A) (P : A -> Type), P x -> forall y : A, x = y -> P y]
+
+   Provided a proof that [x = y], we can create on object of type [P y] from an 
+   existing object of type [P x]. This substitution principle is enough to show
+   that equality is symmetric and transitive. For example we can use 
+   pattern-matching on equality proofs to show:
+ *)
 
 Equations eq_trans {A} (x y z : A) (p : x = y) (q : y = z) : x = z :=
 eq_trans A ?(x) ?(x) ?(x) eq_refl eq_refl := eq_refl.
+
+(** Let us explain the meaning of the non-linear patterns here that we 
+   slipped through in the [equal] example. By pattern-matching on the 
+   equalities, we have unified [x], [y] and [z], hence we determined the 
+   _values_ of the patterns for the variables to be [x]. The [?(x)] 
+   notation is essentially denoting that the pattern is not a candidate 
+   for refinement, as it is determined by another pattern. 
+
+   **** Indexed datatypes
+   
+   Functions on [vector]s provide more stricking examples of this situation.
+   The [vector] family is indexed by a natural number representing the size of 
+   the vector: [[
+Inductive vector (A : Type) : nat -> Type :=
+| Vnil : vector A O
+| Vcons : A -> forall n : nat, vector A n -> vector A (S n) ]]
+
+   The empty vector [Vnil] has size [O] while the cons operation increments 
+   the size by one. Now let us define the usual map on vectors:
+ *)
+
+Equations vmap {A B} (f : A -> B) {n} (v : vector A n) :
+  vector B n :=
+vmap A B f ?(0) Vnil := Vnil ;
+vmap A B f ?(S n) (Vcons a n v) := Vcons (f a) (vmap f v).
+
+(** Here the value of the index representing the size of the vector 
+   is directly determined by the constructor, hence in the case tree
+   we have no need to eliminate [n]. This means in particular that 
+   the function [vmap] does not do any computation with [n], and 
+   the argument could be eliminated in the extracted code.
+   In other words, it provides only _logical_ information about 
+   the shape of [v] but no computational information.
+
+   The [vmap] function works on every member of the [vector] family,
+   but some functions may work only for some subfamilies, for example
+   [vhead]:
+ *)
+
+Equations vhead {A n} (v : vector A (S n)) : A :=
+vhead A ?(S n) (Vcons a n v) := a.
+
+(** The type of [v] ensures that [vhead] can only be applied to 
+   non-empty vectors, moreover the patterns only need to consider 
+   constructors that can produce objects in the subfamily [vector A (S n)],
+   excluding [Vnil]. The pattern-matching compiler uses unification 
+   with the theory of constructors to discover which cases need to 
+   be considered and which are impossible. In this case the failed 
+   unification of [0] and [S n] shows that the [Vnil] case is impossible.
+   This powerful unification engine running under the hood permits to write
+   concise code where all uninteresting cases are handled automatically.
+   
+   Of course the equations and the induction principle are simplified in a 
+   similar way. If we encounter a call to [vhead] in a proof, we can 
+   use the following elimination principle to simplify both the call and the
+   argument which must have the form [Vcons _ _ _]:[[
+forall P : forall (A : Type) (n : nat) (v : vector A (S n)), A -> Prop,
+(forall (A : Type) (n : nat) (a : A) (v : vector A n), 
+  P A n (Vcons a v) a) ->
+forall (A : Type) (n : nat) (v : vector A (S n)), P A n v (vhead v) ]]
+
+*) 
