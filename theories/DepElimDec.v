6f0f26b6b6d0fc165dc7bc2d543e9137bceda0a4
Cyprien Mangin <cyprien.mangin@m4x.org>
2015-05-20 10:35:30 +0200

    Improve generalization with existentials when the goal is non-dependent

--- theories/DepElimDec.v
+++ theories/DepElimDec.v
@@ -33,11 +33,23 @@ Ltac generalize_sig id cont :=
   let id' := fresh id in
   get_signature_pack id id';
   hnf in (value of id'); hnf in (type of id');
-  generalize (@eq_refl _ id' : id' = id') ;
-  unfold id' at 1;
-  clearbody id'; simpl in id'; move id' after id;
-  revert_until id'; rename id' into id;
-    cont id.
+  match goal with
+  | |- context[ id ] =>
+    generalize (@eq_refl _ id' : id' = id') ;
+    unfold id' at 1;
+    clearbody id'; simpl in id'; move id' after id;
+    revert_until id'; rename id' into id;
+      cont id
+  | |- _ =>
+    let id'1 := fresh id' in let id'2 := fresh id' in
+    set (id'2 := projT2 id'); set (id'1 := projT1 id') in id'2;
+    hnf in (value of id'1), (value of id'2);
+    generalize (@eq_refl _ id'1 : id'1 = id'1);
+    unfold id'1 at 1; clearbody id'2 id'1;
+    clear id' id; compute in id'2;
+    rename id'2 into id;
+      cont id'1
+  end.
 
 Ltac generalize_sig_dest id :=
   generalize_sig id ltac:(fun id => decompose_exists id).
