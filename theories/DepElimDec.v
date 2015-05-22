d4611d5754d3b8aac03e77ef3cdeff8b471e3b30
Cyprien Mangin <cyprien.mangin@m4x.org>
2015-05-22 15:52:50 +0200

    Fix the check for dependent goal in generalize_sig
    There is a lot of code duplication in this tactic, but
    I'm not sure how to reduce it.

--- theories/DepElimDec.v
+++ theories/DepElimDec.v
@@ -30,25 +30,29 @@ Ltac decompose_exists id := hnf in id ;
 (** Dependent generalization using existentials only. *)
 
 Ltac generalize_sig id cont :=
-  let id' := fresh id in
-  get_signature_pack id id';
-  hnf in (value of id'); hnf in (type of id');
+  (* Check if goal is dependent or not. *)
   match goal with
-  | |- context[ id ] =>
+  | |- _ => try (try (clear id; gfail 1); fail 1);
+    let id' := fresh id in
+    get_signature_pack id id';
+    hnf in (value of id'), (type of id');
+    let id'1 := fresh id' in let id'2 := fresh id' in
+    set (id'2 := projT2 id'); set (id'1 := projT1 id') in id'2;
+    hnf in (value of id'1), (value of id'2), (type of id'2);
+    generalize (@eq_refl _ id'1 : id'1 = id'1);
+    unfold id'1 at 1; clearbody id'2 id'1;
+    clear id' id; rename id'2 into id;
+    unfold projT2, projT1 in id;
+      cont id'1
+  | |- _ =>
+    let id' := fresh id in
+    get_signature_pack id id';
+    hnf in (value of id'), (type of id');
     generalize (@eq_refl _ id' : id' = id') ;
     unfold id' at 1;
     clearbody id'; simpl in id'; move id' after id;
     revert_until id'; rename id' into id;
       cont id
-  | |- _ =>
-    let id'1 := fresh id' in let id'2 := fresh id' in
-    set (id'2 := projT2 id'); set (id'1 := projT1 id') in id'2;
-    hnf in (value of id'1), (value of id'2);
-    generalize (@eq_refl _ id'1 : id'1 = id'1);
-    unfold id'1 at 1; clearbody id'2 id'1;
-    clear id' id; compute in id'2;
-    rename id'2 into id;
-      cont id'1
   end.
 
 Ltac generalize_sig_dest id :=
