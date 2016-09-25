651b0cd5c7eb4c6f732a91be2a738c853d900521
Matthieu Sozeau <mattam@mattam.org>
2016-09-25 14:57:56 +0200

    [forward c by tac] tactic definition

--- theories/Init.v
+++ theories/Init.v
@@ -84,3 +84,12 @@ End IdTheory.
 
 (** Forward reference for the NoConfusion tactic. *)
 Ltac noconf H := congruence || injection H; intros; subst.
+
+(** Such a useful tactic it should be part of the stdlib. *)
+Ltac forward_gen H tac :=
+  match type of H with
+  | ?X -> _ => let H' := fresh in assert (H':X) ; [tac|specialize (H H'); clear H']
+  end.
+
+Tactic Notation "forward" constr(H) := forward_gen H ltac:(idtac).
+Tactic Notation "forward" constr(H) "by" tactic(tac) := forward_gen H tac.
