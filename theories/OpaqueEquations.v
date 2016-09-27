d90b586201f62415b83f501e2bcccd7a2e4c196b
Matthieu Sozeau <mattam@mattam.org>
2016-09-27 15:12:19 +0200

    Add two files to set/unset transparency of constants
    They are used during simplification.

---
+++ theories/OpaqueEquations.v
@@ -0,0 +1,19 @@
+(**********************************************************************)
+(* Equations                                                          *)
+(* Copyright (c) 2009-2015 Matthieu Sozeau <matthieu.sozeau@inria.fr> *)
+(**********************************************************************)
+(* This file is distributed under the terms of the                    *)
+(* GNU Lesser General Public License Version 2.1                      *)
+(**********************************************************************)
+
+(** This module sets the set constants of Equations to opaque mode so
+  that computation is not possible inside Coq, the tactics need this
+  to solve obligations. *)
+
+From Equations Require Import DepElim.
+
+Global Opaque simplification_existT2 simplification_existT2_dec
+       simplification_sigma2 simplification_sigma2_dec
+       simplification_sigma2_dec_point
+       simplification_heq simplification_K simplification_K_dec
+       simplify_ind_pack simplified_ind_pack Id_simplification_sigma2.
