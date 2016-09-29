83d76e04c6dbd2b83aa7ad91656ce63d2b3b7404
Matthieu Sozeau <mattam@mattam.org>
2016-09-29 08:46:24 +0200

    Forgot the derive files...

---
+++ src/derive.ml
@@ -0,0 +1,42 @@
+(**********************************************************************)
+(* Equations                                                          *)
+(* Copyright (c) 2009-2016 Matthieu Sozeau <matthieu.sozeau@inria.fr> *)
+(**********************************************************************)
+(* This file is distributed under the terms of the                    *)
+(* GNU Lesser General Public License Version 2.1                      *)
+(**********************************************************************)
+
+open Term
+
+type derive_record =
+  { derive_name : string;
+    derive_fn : Globnames.global_reference -> unit }
+
+let make_derive fn s =
+  let env = Global.env () in
+  let sigma = Evd.from_env env in
+  let sigma, c = Evarutil.new_global sigma s in
+  fn env sigma c
+
+let make_derive_ind fn s =
+  let fn env sigma c =
+    match kind_of_term c with
+    | Ind i -> fn env sigma i
+    | _ -> Errors.error "Expected an inductive type"
+  in make_derive fn s
+                 
+let table = ref CString.Map.empty
+    
+let register_derive d =
+  table := CString.Map.add d.derive_name d.derive_fn !table
+
+let get_derive d =
+  try CString.Map.find d !table
+  with Not_found -> Errors.error ("No derive declared for " ^ d)
+                                 
+let derive_one d grs =
+  let fn = get_derive d in
+  List.iter fn grs
+
+let derive ds grs =
+  List.iter (fun d -> derive_one d grs) ds
