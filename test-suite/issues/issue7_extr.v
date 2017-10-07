e204891b4843a07cfe74b48bec96f569f5dc13ef
Matthieu Sozeau <mattam@mattam.org>
2017-10-07 15:34:57 +0200

    issue7 extraction test: it really is just a general fixpoint with no runtime test on indices.

---
+++ test-suite/issues/issue7_extr.v
@@ -0,0 +1,12 @@
+Require Import Equations.Equations Utf8.
+Add Rec LoadPath ".." as Top.
+Require Import issue7.
+
+
+Extraction Inline apply_noConfusion.
+Extraction Inline simplify_ind_pack.
+Extraction Inline simplify_ind_pack_inv.
+Extraction Inline eq_simplification_sigma1_dep_dep.
+
+Extraction Inline Equations.EqDec.K_dec.
+Extraction myComp.
