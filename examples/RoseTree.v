c0cfd5dbd516c69f7a359542083b390b8c9ee8ea
Matthieu Sozeau <mattam@mattam.org>
2017-11-22 00:24:11 +0100

    Use funelim in RoseTree

--- examples/RoseTree.v
+++ examples/RoseTree.v
@@ -11,7 +11,7 @@ Require Import Omega Utf8.
 
 Require Import List.
 
-Equations(nocomp) map_In {A B : Type}
+Equations map_In {A B : Type}
      (l : list A) (f : forall (x : A), In x l -> B) : list B :=
   map_In nil _ := nil;
   map_In (cons x xs) f := cons (f x _) (map_In xs (fun x H => f x _)).
@@ -19,25 +19,22 @@ Equations(nocomp) map_In {A B : Type}
 Lemma map_In_spec {A B : Type} (f : A -> B) (l : list A) :
   map_In l (fun (x : A) (_ : In x l) => f x) = List.map f l.
 Proof.
-  (* Why does funelim not work here? *)
-  (* funelim (map_In l (fun (x : A) (_ : In x l) => f x)). *)
-  induction l; simpl; trivial.
-  rewrite map_In_equation_2. rewrite IHl. trivial.
+  remember (fun (x : A) (_ : In x l) => f x) as g.
+  funelim (map_In l g); rewrite ?H; trivial.
 Qed.
   
 Section list_size.
   Context {A : Type} (f : A -> nat).
-  Equations(nocomp) list_size (l : list A) : nat :=
+  Equations list_size (l : list A) : nat :=
   list_size nil := 0;
   list_size (cons x xs) := S (f x + list_size xs).
   
   Lemma In_list_size:
     forall x xs, In x xs -> f x < S (list_size xs).
   Proof.
-    intros. induction xs; simpl in *; try contradiction; 
-     rewrite list_size_equation_2; destruct H.
+    intros. funelim (list_size xs); simpl in *; destruct H0.
     * subst; omega.
-    * intuition.
+    * specialize (H _ H0). intuition.
   Qed.
 End list_size.
 
@@ -62,7 +59,7 @@ Module RoseTree.
               (Pnil : P (node nil))
               (Pnode : forall x xs, P x -> P (node xs) -> P (node (cons x xs))).
               
-      Equations(nocomp noind) elim (r : t) : P r :=
+      Equations(noind) elim (r : t) : P r :=
       elim r by rec r (MR lt size) :=
       elim (leaf a) := Pleaf a;
       elim (node nil) := Pnil;
@@ -76,7 +73,7 @@ Module RoseTree.
       Defined.
     End elimtree.
 
-    Equations(nocomp) elements (r : t) : list A :=
+    Equations elements (r : t) : list A :=
     elements l by rec r (MR lt size) :=
     elements (leaf a) := [a];
     elements (node l) := concat (map_In l (fun x H => elements x)).
@@ -86,7 +83,7 @@ Module RoseTree.
       apply In_list_size. auto.
     Defined.
       
-    Equations(nocomp) elements_def (r : t) : list A :=
+    Equations elements_def (r : t) : list A :=
     elements_def (leaf a) := [a];
     elements_def (node l) := concat (List.map elements l).
     Lemma elements_equation (r : t) : elements r = elements_def r.
@@ -100,7 +97,7 @@ Module RoseTree.
     Hint Extern 4 (MR _ _ _ _) => abstract (repeat red; simpl in *; omega) : rec_decision.
 
     (* Nested rec *) 
-    Equations(nocomp) elements' (r : t) : list A :=
+    Equations elements' (r : t) : list A :=
     elements' l by rec r (MR lt size) :=
     elements' (leaf a) := [a];
     elements' (node l) := fn l hidebody
@@ -114,7 +111,7 @@ Module RoseTree.
       abstract (simpl; omega).
     Defined.
 
-    Equations(nocomp) elements'_def (r : t) : list A :=
+    Equations elements'_def (r : t) : list A :=
     elements'_def (leaf a) := [a];
     elements'_def (node l) := concat (List.map elements' l).
 
@@ -132,15 +129,15 @@ Module RoseTree.
   Section fns.
     Context {A B : Set} (f : A -> B) (g : B -> A -> B) (h : A -> B -> B).
     
-    Equations(nocomp) map (r : t A) : t B :=
+    Equations map (r : t A) : t B :=
     map (leaf a) := leaf (f a);
     map (node l) := node (List.map map l).
 
-    Equations(nocomp) fold (acc : B) (r : t A) : B :=
+    Equations fold (acc : B) (r : t A) : B :=
     fold acc (leaf a) := g acc a;
     fold acc (node l) := List.fold_left fold l acc.
 
-    Equations(nocomp) fold_right (r : t A) (acc : B) : B :=
+    Equations fold_right (r : t A) (acc : B) : B :=
     fold_right (leaf a) acc := h a acc;
     fold_right (node l) acc := List.fold_right fold_right acc l.
   End fns.    
