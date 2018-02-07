daf543a754b720833485bd27b36c662631c8f707
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2018-02-07 15:07:41 +0100

    Fix for coq/coq#891 (check undeclared universes)

--- src/equations_common.ml
+++ src/equations_common.ml
@@ -174,8 +174,8 @@ let make_definition ?opaque ?(poly=false) evd ?types b =
   in
   let evm, nf = Evarutil.nf_evars_and_universes evm in
   let body = EConstr.to_constr evm b and typ = Option.map (EConstr.to_constr evm) types in
-  let used = Univops.universes_of_constr env body in
-  let used' = Option.cata (Univops.universes_of_constr env) Univ.LSet.empty typ in
+  let used = Typeops.universe_levels_of_constr env body in
+  let used' = Option.cata (Typeops.universe_levels_of_constr env) Univ.LSet.empty typ in
   let used = Univ.LSet.union used used' in
   let evm = Evd.restrict_universe_context evm used in
   evd := evm;
