18395c26a17b418fb30a34721fdd488947354d64
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2018-06-12 19:19:20 +0200

    Fix issue #113: missing List.rev in mutual nested fixpoint computation.

---
+++ test-suite/issues/issue113.v
@@ -0,0 +1,24 @@
+From Coq.Lists Require Import List.
+From Equations Require Import Equations.
+
+Inductive foo :=
+| List : list foo -> foo.
+
+Equations (struct t) do_foo1 (t: foo) : nat := {
+  do_foo1 (List ss) := do_foo1_list ss }
+
+  where (struct ts)
+    do_foo1_list (ts:list foo) : nat := {
+    do_foo1_list nil := 0;
+    do_foo1_list (cons t ts) := do_foo1 t + do_foo1_list ts }
+
+  where (struct t) do_foo2 (t : foo) : nat := {
+
+  do_foo2 (List nil) := 0;
+  do_foo2 (List (cons t ts)) := do_foo2 t }
+
+  where (struct ts)
+    do_foo2_list (ts:list foo) : nat := {
+    do_foo2_list nil := 0;
+    do_foo2_list (cons t ts) := do_foo2_list ts }.
+Definition check := do_foo1_elim.
