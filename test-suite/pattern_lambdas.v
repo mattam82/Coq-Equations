b502a1b7117462e751e9125b125ce039761bad34
Matthieu Sozeau <mattam@mattam.org>
2018-11-30 17:25:53 +0100

    Real example of pattern-matching lambda

--- test-suite/pattern_lambdas.v
+++ test-suite/pattern_lambdas.v
@@ -19,25 +19,10 @@ Equations(noind) f' (n : nat) : nat :=
   f' n := test n (#{ | 0 => true ;
                      | S n => false }).
 
-
-
-
-Definition pattern_lambda {A} (x : A) := x.
-
-Notation "'λ' ' x" := (pattern_lambda (fun 'x => x)) (at level 0, x pattern).
-
-
-
-
 Equations decideeq (b b' : bool) : (b = b') + (~ b = b') :=
   decideeq true true := inl eq_refl;
   decideeq false false := inl eq_refl;
-  decideeq false true := inr _;
-  decideeq true false := inr _.
+  decideeq false true := inr (#{ | x :=! x });
+  decideeq true false := inr (#{ | x :=! x }).
 
-
-_≟_ : Decidable {A = Bool} _≡_
-true  ≟ true  = yes refl
-false ≟ false = yes refl
-true  ≟ false = no λ ()
-false ≟ true  = no λ ()
+Fail Notation "'λ()'" := (#{ | x :=! x }).
