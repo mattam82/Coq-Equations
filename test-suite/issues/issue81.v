78df9178544e1f8f4d8e9cb9c6e5645b594ba02a
Matthieu Sozeau <mattam@mattam.org>
2019-01-03 17:49:34 +0100

    Add closed bugs to the test-suite

---
+++ test-suite/issues/issue81.v
@@ -0,0 +1,7 @@
+Require Import Coq.Lists.List.
+Import ListNotations.
+From Equations Require Import Equations.
+
+Equations app {A} (l l' : list A) : list A :=
+app [] l' := l' ;                                  (* <- here notations work *)
+app (a :: l) l' := a :: (app l l').
