6be634103588d463fff8e97689a27b1b4a9b5230
Matthieu Sozeau <mattam@mattam.org>
2019-01-03 20:17:03 +0100

    Issue #116 is not completely solved

--- test-suite/issues/issue116.v
+++ test-suite/issues/issue116.v
@@ -3,30 +3,28 @@ From Equations Require Import Equations.
 
 Inductive foo :=
 | Sum : list foo -> foo.
-Set Equations Debug.
 
-Equations (struct t) do_foo1 (t: foo) : nat := {
+Equations do_foo1 (t: foo) : nat (* by struct t *) := {
   do_foo1 (Sum u) :=
-        do_foo1_list u }
+        do_foo1_list u
 
-  where (struct ts)
-    do_foo1_list (ts:list foo) : nat := {
+
+  (* where do_foo2 (t : foo) : nat by struct t := { *)
+  (* do_foo2 (Sum nil) := 0; *)
+  (* do_foo2 (Sum (l :: tl)) := do_foo2 l } *)
+
+  where
+    do_foo1_list (ts:list foo) : nat by struct ts := {
     do_foo1_list nil := 0;
-    do_foo1_list (cons t ts) := do_foo2 t + (do_foo1_list ts)}
+    do_foo1_list (cons t ts) := do_foo1 t + (do_foo1_list ts)} }.
 
-  where (struct fs) do_foo1_list2 (fs : list foo) : nat :=
-          { do_foo1_list2 nil := 0;
-            do_foo1_list2 (cons t ts) := do_foo1_list2 ts}
 
-  where (struct t) do_foo2 (t : foo) : nat := {
-  do_foo2 (Sum u) := do_foo2_list u }
-
-  where (struct ts)
-    do_foo2_list (ts:list foo) : nat := {
-    do_foo2_list nil := 0;
-    do_foo2_list (cons t ts) <= (do_foo2_list ts) => {
-      do_foo2_list (cons t _) _ := (do_foo1 t) + (do_foo2 t)}}.
+  (* where *)
+  (*   do_foo2_list (ts:list foo) : nat := { *)
+  (*   do_foo2_list nil := 0; *)
+  (*   do_foo2_list (cons t ts) with (do_foo2_list ts) => { *)
+  (*     do_foo2_list (cons t _) _ := (do_foo1 t) + (do_foo2 t)}} }. *)
 
 Axiom cheat : forall {A}, A.
-Next Obligation. apply cheat. Defined.
+(* Next Obligation. apply cheat. Defined. *)
 Definition check := do_foo1_elim.
