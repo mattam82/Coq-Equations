5d56bc601381ee16e8094396aae43c82a931846c
Matthieu Sozeau <mattam@mattam.org>
2019-01-26 16:02:41 +0100

    Checked issue #116 is fixed as well

--- test-suite/issues/issue116.v
+++ test-suite/issues/issue116.v
@@ -6,25 +6,23 @@ Inductive foo :=
 
 Equations do_foo1 (t: foo) : nat (* by struct t *) := {
   do_foo1 (Sum u) :=
-        do_foo1_list u
+        do_foo1_list u }
 
 
-  (* where do_foo2 (t : foo) : nat by struct t := { *)
-  (* do_foo2 (Sum nil) := 0; *)
-  (* do_foo2 (Sum (l :: tl)) := do_foo2 l } *)
+  where do_foo2 (t : foo) : nat by struct t := {
+  do_foo2 (Sum nil) := 0;
+  do_foo2 (Sum (l :: tl)) := do_foo2 l }
 
   where
     do_foo1_list (ts:list foo) : nat by struct ts := {
     do_foo1_list nil := 0;
-    do_foo1_list (cons t ts) := do_foo1 t + (do_foo1_list ts)} }.
+    do_foo1_list (cons t ts) := do_foo1 t + (do_foo1_list ts)}
 
 
-  (* where *)
-  (*   do_foo2_list (ts:list foo) : nat := { *)
-  (*   do_foo2_list nil := 0; *)
-  (*   do_foo2_list (cons t ts) with (do_foo2_list ts) => { *)
-  (*     do_foo2_list (cons t _) _ := (do_foo1 t) + (do_foo2 t)}} }. *)
+  where
+    do_foo2_list (ts:list foo) : nat := {
+    do_foo2_list nil := 0;
+    do_foo2_list (cons t ts) with (do_foo2_list ts) => {
+      do_foo2_list (cons t _) _ := (do_foo1 t) + (do_foo2 t)}}.
 
-Axiom cheat : forall {A}, A.
-(* Next Obligation. apply cheat. Defined. *)
 Definition check := do_foo1_elim.
