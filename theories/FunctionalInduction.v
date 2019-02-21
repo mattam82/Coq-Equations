f1c7a3b298834bcf0522ac3c1c132440ef742c4a
Matthieu Sozeau <mattam@mattam.org>
2019-02-22 00:04:51 +0100

    Add apply_funelim tactic to FunctionalInduction

--- theories/FunctionalInduction.v
+++ theories/FunctionalInduction.v
@@ -141,6 +141,36 @@ Ltac funelim_sig_tac c tac :=
 
 Ltac funelim c := funelim_sig_tac c ltac:(fun _ => idtac).
 
+Ltac apply_args c elimc k :=
+    match c with
+    | _ ?a ?b ?c ?d ?e ?f => k uconstr:(elimc a b c d e f)
+    | _ ?a ?b ?c ?d ?e => k uconstr:(elimc a b c d e)
+    | _ ?a ?b ?c ?d => k uconstr:(elimc a b c d)
+    | _ ?a ?b ?c => k uconstr:(elimc a b c)
+    | _ ?a ?b => k uconstr:(elimc a b)
+    | _ ?a => k uconstr:(elimc a)
+    end.
+
+Ltac get_first_elim c :=
+  match c with
+  | ?f ?a ?b ?c ?d ?e ?f => get_elim (f a b c d e f)
+  | ?f ?a ?b ?c ?d ?e => get_elim (f a b c d e)
+  | ?f ?a ?b ?c ?d => get_elim (f a b c d)
+  | ?f ?a ?b ?c => get_elim (f a b c)
+  | ?f ?a ?b => get_elim (f a b)
+  | ?f ?a => get_elim (f a)
+  end.
+
+(** An alternative tactic that does not generalize over the arguments.
+    BEWARE: It might render the goal unprovable. *)
+Ltac apply_funelim c :=
+  let elimc := get_first_elim c in
+  let elimfn := match elimc with fun_elim (f:=?f) => constr:(f) end in
+  let elimn := match elimc with fun_elim (n:=?n) => constr:(n) end in
+  let elimt := make_refine elimn elimc in
+  apply_args c elimt ltac:(fun elimc =>
+                             unshelve refine_ho elimc; cbv beta).
+
 (** A special purpose database used to prove the elimination principle. *)
 
 Create HintDb funelim.
