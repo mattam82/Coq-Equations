cfa964c021b2d86059a2ea8d2bc4f73ed66a4e82
Matthieu Sozeau <mattam@mattam.org>
2019-03-19 16:50:16 +0100

    Fix bug #172, covering was raising an uncaught Not_found

---
+++ test-suite/issues/issue172.v
@@ -0,0 +1,17 @@
+Set Implicit Arguments.
+
+From Equations Require Export Equations.
+Require Export Equations.Prop.Subterm.
+
+(* Module KAxiom. *)
+
+(** By default we disallow the K axiom, but it can be set. *)
+
+Set Equations With UIP.
+
+(** In this case the following definition uses the [K] axiom just imported. *)
+Axiom uip : forall A, UIP A.
+Existing Instance uip.
+Equations K {A} (x : A) (P : x = x -> Type) (p : P eq_refl)
+          (H : x = x) : P H :=
+  K P p eq_refl := p.
