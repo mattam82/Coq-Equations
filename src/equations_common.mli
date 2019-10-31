a7879e2c4269fb4d3c8a1cb9cc34535cd6ef8afe
Matthieu Sozeau <mattam@mattam.org>
2019-10-31 14:55:35 +0100

    Fix proof_entry move to proof_global

--- src/equations_common.mli
+++ src/equations_common.mli
@@ -147,7 +147,7 @@ val make_definition :
   ?opaque:'a ->
   ?poly:bool ->
   Evd.evar_map ->
-  ?types:constr -> constr -> Evd.evar_map * Evd.side_effects Declare.proof_entry
+  ?types:constr -> constr -> Evd.evar_map * Evd.side_effects Proof_global.proof_entry
 
 (** Declares a constant relative to an evar_map.
 
