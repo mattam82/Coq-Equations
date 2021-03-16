11af0be7d06f81f080e13ea3b7e6607a858ac665
Matthieu Sozeau <mattam@mattam.org>
2021-03-16 22:47:10 +0100

    Fix test-suite

--- test-suite/issues/issue346.v
+++ test-suite/issues/issue346.v
@@ -3,8 +3,6 @@ Definition tyty := Type -> Type.
 Inductive X : tyty := | K : X nat.
 Derive NoConfusion for X.
 Derive Signature for X.
-Derive NoConfusionHom for X.
 Derive Subterm for X.
-Derive EqDec for X.
 
 Check (NoConfusionPackage_X : NoConfusionPackage (sigma (fun x => X x))).
