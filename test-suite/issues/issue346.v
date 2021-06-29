d3d9cfd848861487eb7beb9d00f841332a6375fd
Emilio Jesus Gallego Arias <e+git@x80.org>
2021-06-29 21:30:08 +0200

    [test-suite] Fix open obligations in test-suite case.
    Fixes #409
    
    [sorry I was lazy and used dependent induction]

--- test-suite/issues/issue346.v
+++ test-suite/issues/issue346.v
@@ -1,10 +1,33 @@
+From Coq Require Import Program.
 From Equations Require Import Equations.
 Definition tyty := Type -> Type.
 Inductive X : tyty := | K : X nat.
+
 Derive NoConfusion for X.
 Derive Signature for X.
+
 Derive NoConfusionHom for X.
+
+Next Obligation.
+now dependent induction a; dependent induction b.
+Defined.
+
+Next Obligation.
+dependent induction a; dependent induction b; simpl.
+now compute; rewrite JMeq_eq_refl; destruct e.
+Defined.
+
+Next Obligation.
+dependent induction b; simpl.
+now compute; rewrite JMeq_eq_refl.
+Defined.
+
 Derive Subterm for X.
+
 Derive EqDec for X.
+
+Next Obligation.
+now destruct x; dependent induction y; left.
+Defined.
 
 Check (NoConfusionPackage_X : NoConfusionPackage (sigma (fun x => X x))).
