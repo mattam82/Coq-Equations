4c618ba4f7d2f0a87984c1581c2f16d23d7e6ec6
Matthieu Sozeau <mattam@mattam.org>
2022-02-07 00:27:29 +0100

    Allow passing dependent elimination patterns to tactic notations

---
+++ test-suite/custom_depelim.v
@@ -0,0 +1,21 @@
+From Equations Require Import Equations.
+
+Tactic Notation "delim" constr(c) "as" elim_patterns_or_var(p) :=
+  dependent elimination c as p.
+
+Inductive fin : nat -> Set :=
+ | fin0 n : fin (S n)
+ | finS n : fin n -> fin (S n).
+
+Lemma foo : forall n, fin n -> fin n.
+Proof.
+  intros n f.
+  delim f as [fin0 k|finS k f'].
+  - exact (fin0 k).
+  - exact (finS k f').
+Qed.
+
+
+
+
+
