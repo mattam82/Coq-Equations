7f883b49ce4a8233492f280ec7d9872d273fdced
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2023-07-29 22:13:08 +0200

    Plug the Global.set_strategy used for registration on the section mechanism.

--- src/equations.ml
+++ src/equations.ml
@@ -45,7 +45,7 @@ let define_unfolding_eq ~pm env evd flags p unfp prog prog' ei hook =
   let funfc = e_new_global evd info'.term_id in
   let unfold_eq_id = add_suffix (program_id unfp) "_eq" in
   let hook_eqs _ pm =
-    Global.set_strategy (ConstKey funf_cst) Conv_oracle.transparent;
+    Redexpr.set_strategy false [Conv_oracle.transparent, [EvalConstRef funf_cst]];
     let () = (* Declare the subproofs of unfolding for where as rewrite rules *)
       let decl _ (_, id, _) =
         let gr =
